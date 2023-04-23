ec9ad54994ebe7378f3108a1e80144ac18d37d00
Nick Grover <nickg96@gmail.com>
2023-04-23 00:12:39 -0400

    magic 8 ball program push

--- src/main/java/Magic8Ball.java
+++ src/main/java/Magic8Ball.java
@@ -8,6 +8,52 @@ public class Magic8Ball {
 
     // 1. Make a main method that includes all the steps below
 
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Boolean runBall = true;
+        while (runBall) {
+            System.out.println("Enter a question for the magic 8-ball to answer (Enter 'stop' to end)!: ");
+            String userQuestion = scanner.nextLine();
+            if (userQuestion.equalsIgnoreCase("stop")) {
+                System.out.println("Thank you for playing! I hope you got what you were looking for.");
+                break;
+            }
+
+            int randomNumber = new Random().nextInt(9);
+
+            if (randomNumber == 0) {
+                System.out.println("Yes");
+            }
+            if (randomNumber == 1) {
+                System.out.println("No");
+            }
+            if (randomNumber == 2) {
+                System.out.println("Maybe if you are lucky!");
+            }
+            if (randomNumber == 3) {
+                System.out.println("It's a lock my friend");
+            }
+            if (randomNumber == 4) {
+                System.out.println("There was never a chance");
+            }
+            if (randomNumber == 5) {
+                System.out.println("Unfortunately, I don't think so");
+            }
+            if (randomNumber == 6) {
+                System.out.println("I have a really good feeling for you...");
+            }
+            if (randomNumber == 7) {
+                System.out.println("Oh my god. No way...");
+            }
+            if (randomNumber == 8) {
+                System.out.println("Hmmmm please ask me again");
+            }
+            System.out.println();
+        }
+
+
+    }
+
 
     // 2. Get the user to enter a question for the magic 8-ball to answer
     //    Scanner scanner = new Scanner(System.in);
